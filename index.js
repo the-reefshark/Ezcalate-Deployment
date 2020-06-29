5d8fdd6fdd5536146362436923c2a5d81a831f56
rishabhpaliwal <rishabh92k@gmail.com>
2020-06-29 11:17:30 +0800

    Updated Proxy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const todolist_model = require('./todolist_model')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ezcalate.herokuapp.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
