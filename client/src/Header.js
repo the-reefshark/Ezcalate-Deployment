0c7d7154ade1b87d8729e0affef96cdaa4b0a6a4
rishabhpaliwal <rishabh92k@gmail.com>
2020-07-27 09:51:05 +0800

    Milestone 3

--- client/src/Header.js
+++ client/src/Header.js
@@ -12,7 +12,7 @@ import Avatar from '@material-ui/core/Avatar'
 import Logo from "./Background Images/Ezcalate_logo.png"
 import Button from '@material-ui/core/Button'
 import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/Menuitem'
+import MenuItem from '@material-ui/core/MenuItem'
 import Fade from '@material-ui/core/Fade'
 
 
